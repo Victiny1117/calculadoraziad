2a97a04db1dabc2007e92fcf9108937f8f0289fe
Victiny1117 <146040261+Victiny1117@users.noreply.github.com>
2025-03-28 06:45:57 -0400

    corregi metodos y añadi algunas cosas que faltaban, la calculadora ahora es funcional

--- lib/logic/calculator.dart
+++ lib/logic/calculator.dart
@@ -1,9 +1,9 @@
 class Calculator {
-  double sum(double num1, double num2) {
+
+    double sum(double num1, double num2) {
     return num1 + num2;
     //suma
   }
-  
   double subs(double num1, double num2) {
     return num1 - num2;
     //resta
@@ -22,5 +22,27 @@ class Calculator {
   double perc(double num1) {
     return num1 / 100;
     //porcentaje
+  }
+
+  double equals(double num1, double num2, String sign) {
+    
+    switch(sign) {
+      case '+':
+      return sum(num1, num2);
+      break;
+      case '-':
+      return subs(num1, num2);
+      break;
+      case '*':
+      return mult(num1, num2);
+      break;
+      case '/':
+      return div(num1, num2);
+      break;
+      case '%':
+      return perc(num1);
+      break;
+    }
+    return 0;
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,49 +8,338 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Calculator calc = Calculator();
-  String screen = '';
+  String screen = '0';
+  String sign = '';
+  double num1 = 0; 
+  double num2 = 0;
 
   void addChar(String char) {
     setState(() {
-      screen += char;
+      if(char == "." && screen == "0") {
+        screen = "0.";
+      } else if (screen == "0") {
+        screen = char;
+      } else {
+        screen += char;
+      }
     });
   }
 
   void eraseLastChar() {
     setState(() {
-      screen = screen.isNotEmpty ? screen.substring(0, screen.length - 1) : '';
+      screen = screen.isNotEmpty ? screen.substring(0, screen.length - 1) : '0';
     });
   }
 
   void deleteEverything() {
     setState(() {
-      screen = '';
+      screen = '0';
+      num1 = 0;
+      num2 = 0;
+      sign = '';
     });
+  }
+
+ double result(double num1, double num2, String sign) {
+    if (sign != '') {
+      return calc.equals(num1, num2, sign);
+    } else {
+      return num1;
+    }
   }
 
   // magia
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Calculadora")),
-      body: Column(
-        children: [
-          Text(screen, style: TextStyle(fontSize: 36)),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 4, // Botones de la calculadora
-              children: [
-                ...['1', '2', '3', '+'].map((btn) => ElevatedButton(
-                  onPressed: () => addChar(btn),
-                  child: Text(btn),
-                )),
-                ElevatedButton(onPressed: eraseLastChar, child: Text('DEL')),
-                ElevatedButton(onPressed: deleteEverything, child: Text('AC')),
-              ],
-            ),
-          ),
-        ],
+      
+
+      appBar: AppBar(
+        title: Text(
+        "Calculadora",
+      style: TextStyle(
+        color:  Color.fromARGB(255, 221, 221, 221),
+            fontWeight: FontWeight.bold,
+        ),
       ),
-    );
+      centerTitle: true,
+      backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+      ),
+      
+      
+      backgroundColor: Color.fromARGB(255, 29, 29, 29),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50, right: 50,
+            child:
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.transparent
+              ),
+              alignment: Alignment.center,
+              child: 
+              Text(screen, style: TextStyle(
+            color: Color.fromARGB(255, 221, 221, 221),
+            fontSize: 36,
+                  ),
+                ),
+              ),
+            ),
+          
+          Positioned(
+            top: 170, left: 60,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("1"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("1"),
+            )
+            ),
+            Positioned(
+            top: 170, left: 140,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("2"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("2"),
+            )
+            ),
+
+            Positioned(
+            top: 170, left: 220,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("3"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("3"),
+            )
+            ),
+
+            Positioned(
+            top: 255, left: 60,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("4"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("4"),
+            )
+            ),
+
+            Positioned(
+            top: 255, left: 140,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("5"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("5"),
+            )
+            ),
+
+            Positioned(
+            top: 255, left: 220,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("6"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("6"),
+            )
+            ),
+
+            Positioned(
+            top: 340, left: 60,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("7"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("7"),
+            )
+            ),
+
+            Positioned(
+            top: 340, left: 140,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("8"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("8"),
+            )
+            ),
+
+            Positioned(
+            top: 340, left: 220,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("9"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("9"),
+            )
+            ),
+
+            Positioned(
+            top: 425, left: 60,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("0"),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("0"),
+            )
+            ),
+
+            Positioned(
+            top: 425, left: 140,
+            child: 
+            FloatingActionButton(
+              onPressed: () => addChar("."),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("."),
+            )
+            ),
+
+            Positioned(
+            top: 425, left: 220,
+            child: 
+            FloatingActionButton(
+              onPressed: () { setState(() {
+                num2 = double.parse(screen);
+                screen = result(num1, num2, sign).toString();
+              }); },
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("="),
+            )
+            ),
+
+            Positioned(
+            top: 170, left: 300,
+            child: 
+            FloatingActionButton(
+              onPressed: () { setState(() {
+                sign = '+'; 
+                num1 = double.parse(screen);
+                screen = "0"; 
+              }); },
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("+"),
+            )
+            ),
+
+            Positioned(
+            top: 255, left: 300,
+            child: 
+            FloatingActionButton(
+              onPressed: () { setState(() {
+                sign = '-'; 
+                num1 = double.parse(screen);
+                screen = "0"; 
+              }); },
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("-"),
+            )
+            ),
+
+            Positioned(
+            top: 340, left: 300,
+            child: 
+            FloatingActionButton(
+              onPressed: () { setState(() {
+                sign = '*'; 
+                num1 = double.parse(screen);
+                screen = "0"; 
+              }); },
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("*"),
+            )
+            ),
+
+            Positioned(
+            top: 425, left: 300,
+            child: 
+            FloatingActionButton(
+              onPressed: () { setState(() {
+                sign = '/'; 
+                num1 = double.parse(screen);
+                screen = "0"; 
+              }); },
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("/"),
+            )
+            ),
+
+            Positioned(
+            top: 510, left: 300,
+            child: 
+            FloatingActionButton(
+              onPressed: () { setState(() {
+                sign = '%'; 
+                num1 = double.parse(screen);
+                screen = "0"; 
+              }); },
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("%"),
+            )
+            ),
+
+            Positioned(
+            top: 510, left: 220,
+            child: 
+            FloatingActionButton(
+              onPressed: () => eraseLastChar(),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("DE"),
+            )
+            ),
+
+            Positioned(
+            top: 510, left: 140,
+            child: 
+            FloatingActionButton(
+              onPressed: () => deleteEverything(),
+              backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+              foregroundColor: const Color.fromARGB(255, 19, 19, 19),
+            child: 
+            Text("CL"),
+            )
+            ),
+         ],
+       ),
+     ); 
   }
 }
